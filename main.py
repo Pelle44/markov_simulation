ea613baf72c11e95ad25124a2b606818698457bb
Pelle44 <p.langfeldt@live.de>
2022-10-20 19:18:36 +0200

    for loop generates random customers now. All customers walk through the supermarket and leave through checkout

--- main.py
+++ main.py
@@ -57,29 +57,52 @@ if __name__ == '__main__':
     supermarket = Supermarket()
     supermarket.open()
 
-    customer = Customer(1, supermarket.time, supermarket.get_entry_location())
-    timestamp = customer.get_last_timestamp()
-    print(f'Customer is at {timestamp.location.name} at time {timestamp.time}')
-
-    customers = [customer]
+    customers = []
 
     # for loop that checks if the supermarket is before 21:50
     #   If so then incerement one minute and simulate generation/movement of customers
     #   Else close store and send current customers to checkout
 
-    transition_matrix = pd.read_csv('data/transition_matrix.csv')
+    transition_matrix = pd.read_csv('transition_matrix.csv')
     transition_matrix.set_index('location', inplace=True)
     transition_matrix = transition_matrix.transpose()
 
+    customers_per_min = {7: 1.6333333333333333, #data should come from data. To implemented
+                        8: 2.35,                
+                        9: 1.5166666666666666,
+                        10: 1.3166666666666667,
+                        11: 1.1166666666666667,
+                        12: 1.3166666666666667,
+                        13: 1.7666666666666666,
+                        14: 1.6166666666666667,
+                        15: 1.3333333333333333,
+                        16: 1.7833333333333334,
+                        17: 1.8833333333333333,
+                        18: 2.1666666666666665,
+                        19: 2.5,
+                        20: 1.6,
+                        21: 0.9333333333333333}
+    
+    
+    id = 1
+    
     for i in range(900):
+        key = supermarket.time.seconds // 3600 #takes hour from time as key for dict
+        no_of_customers = round(np.random.normal(customers_per_min[key])) #generated customers per min
         supermarket.time = supermarket.time + timedelta(minutes=1)
+        for no in range(no_of_customers): #adds generated customers to list
+            customer = Customer(id, supermarket.time, supermarket.get_entry_location())
+            customers.append(customer)
+            id = id + 1
         if supermarket.time < timedelta(hours=21, minutes=50):
             for customer in customers:
                 customer.next_state(transition_matrix, supermarket.time, supermarket.locations)
                 timestamp = customer.get_last_timestamp()
-                print(f'Customer is at {timestamp.location.name} at time {timestamp.time}')
+                if customer.id == 600: # You can test it for random Customers
+                    print(f'{customer.id} is at {timestamp.location.name} at time {timestamp.time}')
 
             # filter out customers at checkout
             customers = filter(lambda customer: customer.get_last_location() != 'checkout', customers)
+            customers = list(customers) # need to set it back to list. Otherwise  can not append
         else:
             supermarket.close()
